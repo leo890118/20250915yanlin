e9b5ee4bc94aecea7bebe1a669029d4fada7e450
yan_lin <u0110009@gmail.com>
2025-09-15 16:12:41 +0800

    main

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-def main():
-    print("Hello from 20250915yanlin!")
+from flask import Flask
 
+app = Flask(__name__)
+
+@app.route("/")
+def undex():
+    return '<h1 style ="color:blue">你好呀!Flask<h1>'
 
 if __name__ == "__main__":
-    main()
+    app.run(debug=True)
