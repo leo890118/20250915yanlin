05f5855aa138a4cca1a6036cdc41ed1de244d270
yan_lin <u0110009@gmail.com>
2025-09-25 15:59:53 +0800

    modify

---
+++ lesson2/lesson3_3_2.py
@@ -0,0 +1,14 @@
+import tools
+
+def main():
+    try:
+        data = tools.download_youbike_data()
+        areas = tools.get_area(data)
+        print(areas)
+    except Exception as e:
+        print("發生錯誤\n{e}")
+    
+   
+
+if __name__ == "__main__":
+    main()
